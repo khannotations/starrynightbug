eb06d122460fca0cbe98b55dcd1aab23697026ab
Rafi Khan <rafi.fa.khan@gmail.com>
2015-06-16 23:18:39 +0200

    routes

--- routes/album.js
+++ routes/album.js
@@ -1,42 +1,74 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var mongoose = require('mongoose');
+var restify = require('express-restify-mongoose');
 var Album = require('../models/Album');
 
 // Just JSON routes
 
-// Index
-router.get('/', function(req, res, next) {
-  Album.find({}, function(err, albums) {
-    console.log(albums);
-    res.send(albums || []);
-  });
+restify.serve(router, Album, {
+  prefix: '/api',
+  version: '/v1',
+  idProperty: 'name',
+  prereq: function(req, res, next) {
+    // TODO: ensure authentication
+    // passport.authenticate('basic', {session: false});
+    return true; 
+  },
+  plural: true,
+  lowercase: true
 });
 
-// Show
-router.get('/:id', function(req, res, next) {
-  res.send(Album.findOne({id: req.params.id}));
+router.get('/api/v1/albums/:id/pictures', function(req, res, next) {
+
 });
 
-// Create
-router.post('/', function(req, res, next) {
-  console.log(req.body, req.params);
-  Album.create(req.params, function(err, album) {
-    res.send(album);
-  });
-});
+// // Index
+// router.get('/', function(req, res, next) {
+//   Album.find({}, function(err, albums) {
+//     res.json(albums || []);
+//   });
+// });
 
-// Update
-router.post('/:id', function(req, res, next) {
-  Album.findOne({id: req.params.id}).update(req.params, function(err, album) {
-    res.send(album);
-  });
-});
+// // Show
+// router.get('/:id', function(req, res, next) {
+//   console.log(req.params.id);
+//   Album.findById(req.params.id, function(err, album) {
+//     if (err) { return next(err); }
+//     res.json(album);
+//   });
+// });
 
-// Delete
-router.delete('/:id', function(req, res, next) {
-  Album.findOne({id: req.params.id}).delete(function(err, album) {
-    res.send(album);
-  });
-});
+// // Create - authentication required
+// router.post('/', passport.authenticate('basic', {session: false}),
+//   function(req, res, next) {
+//     Album.create({name: req.body.name, description: req.body.description},
+//       function(err, album) {
+//         if (err) { return next(err); }
+//         res.json(album);
+//       }
+//     );
+//   }
+// );
+
+// // Update - authentication required
+// router.put('/:id', function(req, res, next) {
+//   Album.findById(req.body.id).update(
+//     {name: req.body.name, description: req.body.description},
+//     function(err, album) {
+//       if (err) { return next(err); }
+//       res.json(album);
+//     }
+//   );
+// });
+
+// // Delete - authentication required
+// router.delete('/:id', function(req, res, next) {
+//   Album.findById(req.params.id).remove(function(err, album) {
+//     if (err) { return next(err); }  
+//     res.json(album);
+//   });
+// });
 
 module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -2,27 +2,21 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+function ensureAuthenticated(req, res, next, callback) {
+  var t = this;
+  
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res, next) {
-  res.redirect('/#/admin');
-});
-
 router.get('/current_user', function(req, res, next) {
   passport.authenticate('basic', {session: false}, function(err, user, info) {
-    if (err) {
-      return next(err)
-    }
-    if (!user) {
-      res.status(401).send(false);
-    }
+    if (err) { return next(err) }
+    if (!user) { return res.status(401).send(false); }
     res.send(true);
-    console.log("USER: ", user);
-    console.log("INFO: ", info);
-    res.send({});
   })(req, res, next);
 });
 
