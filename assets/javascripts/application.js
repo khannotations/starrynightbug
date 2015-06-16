904a4e18b6ab603dd0a5c8ed9601351019df6354
Rafi Khan <rafi.fa.khan@gmail.com>
2015-06-16 23:48:32 +0200

    added html5up parallelism

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -5,8 +5,9 @@
 //= require jquery.scrolly.min.js
 //= require jquery.poptrox.min.js
 //= require skel.min.js
+//= require skel-viewport.min.js
 //= require util.js
-//= require main.js
+//= require parallelism.js
 //= require starry
 //= require_tree .
 
